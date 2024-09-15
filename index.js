f2ccbe2b688b5420530d7b23a4626722ce533a2f
maks <makskilin2004@gmail.com>
2024-09-16 01:08:39 +0500

    1-2 tasks complete

--- index.js
+++ index.js
@@ -11,10 +11,38 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function findAllTODOComments(files){
+    let comments = [];
+    
+    for (let file of files){
+        let comment = '';
+
+        for (let i = 0; i < file.length - 6; i++){
+            if ((file[i] + file[i + 1] + file[i + 2] + file[i + 3] + file[i + 4] + file[i + 5] + file[i + 6]) === ('/' + '/ TODO')){
+                for (let j = i; j < file.length; j++){
+                    if ((file[j]) === '\r'){
+                        comments.push(comment);
+                        comment = '';
+                        break;
+                    }
+
+                    comment += file[j];
+                }
+            }
+        }
+
+    }
+
+    return comments;
+}
+
 function processCommand(command) {
     switch (command) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'show':
+            console.log(findAllTODOComments(getFiles()));
             break;
         default:
             console.log('wrong command');
